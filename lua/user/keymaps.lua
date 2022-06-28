8917dd160c750727feb5c57d56bedb7b74fed7d7
David Ewing <davide@tradefull.com>
2022-06-28 08:37:14 -0400

    Initial configuration with basic keymaps, default options, and packer configured

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,37 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten the function name
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<space>", "<Nop>", opts);
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+-- normal = n
+-- insert = i
+-- visual = v
+-- visual_block = x
+-- term = t
+-- command = c
+
+-- File explorer on left
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Window resize with arrows
+keymap("n", "<C-Up>", ":resize +2<cr>", opts)
+keymap("n", "<C-Down>", ":resize -2<cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<cr>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<cr>", opts)
+
+-- Navigate buffers with "L" and "H"
+keymap("n", "<S-l>", ":bnext<cr>", opts)
+keymap("n", "<S-h>", ":bprevious<cr>", opts)
---
+++ lua/user/options.lua
@@ -0,0 +1,23 @@
+-- :help options
+-- :help quickref   /option-list
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = "yes"
+
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.undofile = true
+
+vim.opt.clipboard = "unnamedplus"               -- allows access to system clipboard?
+
+vim.opt.scrolloff = 8
