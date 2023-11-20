71794b2482cca26049234f9984613a5734213b66
David Ewing <david@ewingsoftware.com>
2023-11-20 15:08:22 -0600

    Updated nvim-tree to use on_attach

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -27,13 +27,6 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
 local function on_attach(bufnr)
   local api = require('nvim-tree.api')
 
@@ -152,12 +145,8 @@ nvim_tree.setup {
   view = {
     width = 30,
   --  height = 30,
-    hide_root_folder = false,
     side = "left",
     -- auto_resize = true,
-    mappings = {
-      custom_only = false,
-    },
     number = false,
     relativenumber = false,
   },
